6d22334fdd74b6d9b53885a22da3c3be51ccf4cb
Sethon <bastianbertram@bastians-mbp.wireless.unimaas.local>
2016-02-23 19:42:07 +0100

    Added incomplete drawRoad method.

--- src/com/project/model/Road.java
+++ src/com/project/model/Road.java
@@ -1,12 +1,16 @@
 package com.project.model;
 
+import java.awt.BasicStroke;
+import java.awt.Stroke;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public class Road {
+	protected final Stroke EDGE_OF_ROAD = new BasicStroke(1f);
 	private Point2D start;
 	private Point2D end;
 	private ArrayList<Car> cars;
+	
 
 	public Road(Point2D start, Point2D end){
 		this.start = start;
@@ -31,5 +35,6 @@ public class Road {
 	public Point2D getEnd(){
 		return this.end;
 	}
+	
 	
 }
--- src/com/project/model/StraightRoad.java
+++ src/com/project/model/StraightRoad.java
@@ -1,11 +1,24 @@
 package com.project.model;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 
 public class StraightRoad extends Road{
 
 	public StraightRoad(Point2D start, Point2D end) {
 		super(start, end);
-			}
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	public void drawRoad(Graphics2D g2){
+		g2.setStroke(EDGE_OF_ROAD);
+		g2.setColor(Color.darkGray);
+
+		//draw the line, mapping coordinates to be "normal" i.e. like math graphs
+		g2.drawLine(x1, y1, x2, y2);
+	}
 
 }
