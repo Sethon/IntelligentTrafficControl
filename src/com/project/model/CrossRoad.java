1c20a25905e0a298520bece1cb30a29946f030fc
Sethon <bastianbertram@bastians-mbp.wireless.unimaas.local>
2016-02-23 19:18:57 +0100

    Added shells for cross roads, curves &  straight roads.

---
+++ src/com/project/model/CrossRoad.java
@@ -0,0 +1,12 @@
+package com.project.model;
+
+import java.awt.geom.Point2D;
+
+public class CrossRoad extends Road{
+
+	public CrossRoad(Point2D start, Point2D end) {
+		super(start, end);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/project/model/StraightRoad.java
@@ -0,0 +1,11 @@
+package com.project.model;
+
+import java.awt.geom.Point2D;
+
+public class StraightRoad extends Road{
+
+	public StraightRoad(Point2D start, Point2D end) {
+		super(start, end);
+			}
+
+}
