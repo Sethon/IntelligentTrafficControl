b8a53121ed54b514c70610b2cfa58fda4d87b545
Sethon <bastianbertram@bastians-macbook-pro.local>
2016-02-25 13:10:24 +0100

    Added two edges for a street for both directions and one edge for a street with one direction

--- src/com/project/map/Map.java
+++ src/com/project/map/Map.java
@@ -16,7 +16,7 @@ public class Map {
 		nodeList.add(new Node(new Point(50,100), false));
 		nodeList.add(new Node(new Point(300, 100), false));
 		nodeList.add(new Node(new Point(550, 100), false));
-		nodeList.add(new Node(new Point(800, 100), false));
+//		nodeList.add(new Node(new Point(800, 100), false));
 		nodeList.add(new Node(new Point(1050, 100), false));
 		
 		nodeList.add(new Node(new Point(50, 290), false));
@@ -27,7 +27,7 @@ public class Map {
 		
 		nodeList.add(new Node(new Point(50, 490), false));
 		nodeList.add(new Node(new Point(300, 490), false));
-		nodeList.add(new Node(new Point(550, 490), false));
+//		nodeList.add(new Node(new Point(550, 490), false));
 		nodeList.add(new Node(new Point(800, 490), false));
 		nodeList.add(new Node(new Point(1050, 490), false));
 		
@@ -37,42 +37,81 @@ public class Map {
 		nodeList.add(new Node(new Point(800, 690), false));
 		nodeList.add(new Node(new Point(1050, 690), false));
 		
+		
 		edgeList.add(new Road(nodeList.get(0), nodeList.get(1),30));
 		edgeList.add(new Road(nodeList.get(1), nodeList.get(0),30));
-
-		edgeList.add(new Road(nodeList.get(0), nodeList.get(5),30));
+		
+		edgeList.add(new Road(nodeList.get(0), nodeList.get(4),30));
+		edgeList.add(new Road(nodeList.get(4), nodeList.get(0),30));
+		
 		edgeList.add(new Road(nodeList.get(1), nodeList.get(2),30));
-		edgeList.add(new Road(nodeList.get(1), nodeList.get(6),30));
+		edgeList.add(new Road(nodeList.get(2), nodeList.get(1),30));
+		
+		edgeList.add(new Road(nodeList.get(1), nodeList.get(5),30));
+		edgeList.add(new Road(nodeList.get(5), nodeList.get(1),30));
+
+		edgeList.add(new Road(nodeList.get(2), nodeList.get(6),30));
+	
 		edgeList.add(new Road(nodeList.get(2), nodeList.get(3),30));
-		edgeList.add(new Road(nodeList.get(2), nodeList.get(7),30));
-		edgeList.add(new Road(nodeList.get(3), nodeList.get(4),30));
-//		edgeList.add(new Road(nodeList.get(3), nodeList.get(8),30));
-		edgeList.add(new Road(nodeList.get(4), nodeList.get(9),30));
+		edgeList.add(new Road(nodeList.get(3), nodeList.get(2),30));
+	
+		edgeList.add(new Road(nodeList.get(3), nodeList.get(8),30));
+		edgeList.add(new Road(nodeList.get(8), nodeList.get(3),30));
+		
+		edgeList.add(new Road(nodeList.get(4), nodeList.get(5),30));
+		edgeList.add(new Road(nodeList.get(5), nodeList.get(4),30));
 		
 		edgeList.add(new Road(nodeList.get(5), nodeList.get(6),30));
-		edgeList.add(new Road(nodeList.get(5), nodeList.get(10),30));
-		edgeList.add(new Road(nodeList.get(6), nodeList.get(7),30));
-		edgeList.add(new Road(nodeList.get(6), nodeList.get(11),30));
-		edgeList.add(new Road(nodeList.get(7), nodeList.get(8),30));
-		edgeList.add(new Road(nodeList.get(7), nodeList.get(12),30));
-		edgeList.add(new Road(nodeList.get(8), nodeList.get(9),30));
-		edgeList.add(new Road(nodeList.get(8), nodeList.get(13),30));
-		edgeList.add(new Road(nodeList.get(9), nodeList.get(14),30));
+		edgeList.add(new Road(nodeList.get(6), nodeList.get(5),30));
 		
-		edgeList.add(new Road(nodeList.get(10), nodeList.get(11),30));
-		edgeList.add(new Road(nodeList.get(10), nodeList.get(15),30));
-//		edgeList.add(new Road(nodeList.get(11), nodeList.get(12),30));
-		edgeList.add(new Road(nodeList.get(11), nodeList.get(16),30));
-		edgeList.add(new Road(nodeList.get(12), nodeList.get(13),30));
-		edgeList.add(new Road(nodeList.get(12), nodeList.get(17),30));
+		edgeList.add(new Road(nodeList.get(6), nodeList.get(7),30));
+		edgeList.add(new Road(nodeList.get(7), nodeList.get(6),30));
+		
+		edgeList.add(new Road(nodeList.get(7), nodeList.get(8),30));
+		edgeList.add(new Road(nodeList.get(8), nodeList.get(7),30));
+		
+		edgeList.add(new Road(nodeList.get(4), nodeList.get(9),30));
+		edgeList.add(new Road(nodeList.get(9), nodeList.get(4),30));
+		
+		edgeList.add(new Road(nodeList.get(5), nodeList.get(10),30));
+		edgeList.add(new Road(nodeList.get(10), nodeList.get(5),30));
+
+		edgeList.add(new Road(nodeList.get(7), nodeList.get(11),30));
+		edgeList.add(new Road(nodeList.get(11), nodeList.get(7),30));
+		
+		edgeList.add(new Road(nodeList.get(8), nodeList.get(12),30));
+		edgeList.add(new Road(nodeList.get(12), nodeList.get(8),30));
+
+		edgeList.add(new Road(nodeList.get(9), nodeList.get(10),30));
+		
+		edgeList.add(new Road(nodeList.get(11), nodeList.get(12),30));
+		edgeList.add(new Road(nodeList.get(12), nodeList.get(11),30));
+
+		edgeList.add(new Road(nodeList.get(9), nodeList.get(13),30));
+		edgeList.add(new Road(nodeList.get(13), nodeList.get(9),30));
+		
+		edgeList.add(new Road(nodeList.get(10), nodeList.get(14),30));
+		edgeList.add(new Road(nodeList.get(14), nodeList.get(10),30));
+
 		edgeList.add(new Road(nodeList.get(13), nodeList.get(14),30));
-		edgeList.add(new Road(nodeList.get(13), nodeList.get(18),30));
-		edgeList.add(new Road(nodeList.get(14), nodeList.get(19),30));
+		edgeList.add(new Road(nodeList.get(14), nodeList.get(13),30));
+
+		edgeList.add(new Road(nodeList.get(14), nodeList.get(15),30));
+		edgeList.add(new Road(nodeList.get(15), nodeList.get(14),30));
 		
 		edgeList.add(new Road(nodeList.get(15), nodeList.get(16),30));
+		edgeList.add(new Road(nodeList.get(16), nodeList.get(15),30));
+		
 		edgeList.add(new Road(nodeList.get(16), nodeList.get(17),30));
-		edgeList.add(new Road(nodeList.get(17), nodeList.get(18),30));
-		edgeList.add(new Road(nodeList.get(18), nodeList.get(19),30));
+		edgeList.add(new Road(nodeList.get(17), nodeList.get(16),30));
+
+		edgeList.add(new Road(nodeList.get(11), nodeList.get(16),30));
+		edgeList.add(new Road(nodeList.get(16), nodeList.get(11),30));
+
+		edgeList.add(new Road(nodeList.get(12), nodeList.get(17),30));
+		edgeList.add(new Road(nodeList.get(17), nodeList.get(12),30));
+
+
 	}
 	
 }
