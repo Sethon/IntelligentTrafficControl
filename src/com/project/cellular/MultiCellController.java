cfd435d642bfab08fd7c2fbb1dab9380f42419b5
Frederik Creemers <frederikcreemers@gmail.com>
2016-03-22 21:52:08 +0100

    changed the way MultiCellControllers work.

--- src/com/project/cellular/MultiCellController.java
+++ src/com/project/cellular/MultiCellController.java
@@ -1,11 +1,15 @@
 package com.project.cellular;
 
+import java.util.ArrayList;
+
 public class MultiCellController {
 	private GridCell center;
 	private GridCell left;
 	private GridCell right;
 	private GridCell up;
 	private GridCell down;
+	private ArrayList<GridCell> incoming = new ArrayList<GridCell>();
+	private ArrayList<GridCell> outgoing = new ArrayList<GridCell>();
 	
 	public MultiCellController(GridCell center, GridCell left, GridCell right, GridCell up, GridCell down,
 			Grid grid, int centerX, int centerY){
@@ -14,5 +18,38 @@ public class MultiCellController {
 		this.right = right;
 		this.up = up;
 		this.down = down;
+		this.setConnections();
 	}
+	
+	private void setConnections(){
+		if (left != null) {
+			if(left.getHorizontalPattern() == CellTypes.RIGHT_ROAD.horizontalPattern){
+				incoming.add(left);
+			} else {
+				outgoing.add(left);
+			}
+		}
+		if (right != null){
+			if (right.getHorizontalPattern() == CellTypes.LEFT_ROAD.horizontalPattern){
+				incoming.add(right);
+			} else {
+				outgoing.add(right);
+			}
+		}
+		if (up != null) {
+			if (up.getVerticalPattern() == CellTypes.DOWN_ROAD.verticalPattern) {
+				incoming.add(up);
+			} else {
+				outgoing.add(up);
+			}
+		}
+		if (down != null) {
+			if (down.getVerticalPattern() == CellTypes.UP_ROAD.verticalPattern) {
+				incoming.add(down);
+			} else {
+				outgoing.add(down);
+			}
+		}
+	}
+	
 }
