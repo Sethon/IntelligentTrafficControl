e067b5e51a256a2ed0456230564b0c1963cb0f3f
Frederik Creemers <frederikcreemers@gmail.com>
2016-05-06 15:55:44 +0200

    added road implementation.

--- src/com/project/model/Road.java
+++ src/com/project/model/Road.java
@@ -1,5 +1,34 @@
 package com.project.model;
 
-public class Road {
+import java.awt.Point;
 
+public class Road {
+	public final Intersection from;
+	public final Intersection to;
+	public final Lane leftLane;
+	public final Lane rightLane;
+	
+	public Road(Intersection from, Intersection to, int fromDirection, int toDirection) {
+		this.from = from;
+		this.to = to;
+		
+		int l = this.getLength();
+		this.leftLane = new Lane(l);
+		this.rightLane = new Lane(l);
+		
+		this.updateConnectingIntersections(fromDirection, toDirection);
+	}
+
+	
+	private void updateConnectingIntersections(int fromDirection, int toDirection){
+		from.setOutgoingRoad(this, fromDirection);
+		to.setIncomingRoad(this, toDirection);
+	}
+	
+	private int getLength() {
+		Point pFrom = from.getPosition();
+		Point pTo = to.getPosition();
+		return (int)(Point.distance(pFrom.getX(), pFrom.getY(), pTo.getX(), pTo.getY()) / 1.5);
+	}
+	
 }
