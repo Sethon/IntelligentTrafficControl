8ce233ea7e8712854660613f0b6842b9e20a23a1
Frederik Creemers <frederikcreemers@gmail.com>
2016-05-09 16:34:26 +0200

    Added Globals class, and connection points for intersections.

--- src/com/project/model/Intersection.java
+++ src/com/project/model/Intersection.java
@@ -14,6 +14,25 @@ public class Intersection {
 	private Road[] inRoads = new Road[4];
 	private Road[] outRoads = new Road[4];
 	
+	private Point2D.Double[] connectionPoints = new Point2D.Double[]{
+			// This array stores how roads connect to the intersection.
+			// The first 2 points where the north facing incoming and outgoing roads connect respectively
+			// The second 2 points do that for the east facing roads, and so on.
+			// These co-ordinates specify how the middle of the road touches the intersection.
+			// The points will be scaled by the road with, inside the getConnectionPoint method.
+			new Point2D.Double(0.5, 0),
+			new Point2D.Double(1.5, 0),
+			
+			new Point2D.Double(2, 0.5),
+			new Point2D.Double(2, 1.5),
+			
+			new Point2D.Double(1.5, 2),
+			new Point2D.Double(0.5, 2),
+			
+			new Point2D.Double(0, 1.5),
+			new Point2D.Double(0, 0.5)
+	};
+	
 	public Intersection(Point2D.Double position){
 		this.position = position;
 	}
@@ -42,5 +61,24 @@ public class Intersection {
 		return position;
 	}
 	
+	public Point2D.Double getConnectionPoint(int direction, boolean incoming){
+		// This method returns the point where the middle of a road touches the intersection.
+		// direction is one of the constants defined at the top of this class, incoming is
+		// whether it is the incoming or outgoing road.
+		int i = direction * 2 + (incoming ? 0 : 1);
+		Point2D res = connectionPoints[i];
+		return new Point2D.Double(res.getX() * Globals.LANE_WIDTH, res.getY() * Globals.LANE_WIDTH);
+	}
 	
+	public Point2D.Double getConnectionPoint(Road road){
+		for(int direction = 0; direction < 4; direction++){
+			if(inRoads[direction] == road){
+				return getConnectionPoint(direction, true);
+			}
+			if(outRoads[direction] == road){
+				return getConnectionPoint(direction, false);
+			}
+		}
+		return null;
+	}
 }
