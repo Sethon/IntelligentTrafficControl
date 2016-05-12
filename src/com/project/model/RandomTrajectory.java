24978c75e81256c23728b24504a5c98fc2a4671b
Frederik Creemers <frederikcreemers@gmail.com>
2016-05-13 01:00:26 +0200

    move the code for handling intersections to the base Trajectory class.

--- src/com/project/model/RandomTrajectory.java
+++ src/com/project/model/RandomTrajectory.java
@@ -3,27 +3,10 @@ package com.project.model;
 import java.util.Random;
 
 public class RandomTrajectory extends Trajectory{
-	private Lane targetLane = null;
-	private boolean onIntersection = false;
 	private Random random = new Random();
 	
-	// This trajectory  just picks a road at random at each
-	public Lane getNextLane() {
-		if(onIntersection){
-			// our next lane is the road we've previously selected
-			onIntersection = false;
-			return targetLane;
-		}else{
-			// we're going towards an intersection, pick a direction to leave it.
-			onIntersection = true;
-			targetLane = getTargetLane(currentLane.road.to);
-			//TODO get the internal lane that connects the current road to the target
-			return null;
-		}
-	}
-	
-	private Lane getTargetLane(Intersection intersection){
-		//Pickk a random lane from which we wat to leave the intersection
+	protected Lane getTargetLane(Intersection intersection){
+		//Pick a random lane from which we wat to leave the intersection
 		Road cur = currentLane.road;
 		Road option1;
 		Road option2;
--- src/com/project/model/Trajectory.java
+++ src/com/project/model/Trajectory.java
@@ -1,9 +1,30 @@
 package com.project.model;
 
+import java.util.Random;
+
 public abstract class Trajectory {
 	public Lane currentLane;
 	
-	public abstract Lane getNextLane();
+	private Lane targetLane = null;
+	private boolean onIntersection = false;
+	
+	// This trajectory just picks a road at random at each
+	public Lane getNextLane() {
+		if(onIntersection){
+			// our next lane is the road we've previously selected
+			onIntersection = false;
+			return targetLane;
+		}else{
+			// we're going towards an intersection, pick a direction to leave it.
+			onIntersection = true;
+			targetLane = getTargetLane(currentLane.road.to);
+			//TODO get the internal lane that connects the current road to the target
+			return null;
+		}
+	}
+	
+	//This method, given an intersection, should return on which lane it wants to leave the intersection.
+	protected abstract Lane getTargetLane(Intersection inter);
 	
 	public void updateLane(){
 		currentLane = getNextLane();
