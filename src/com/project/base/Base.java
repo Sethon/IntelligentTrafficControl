a146397bc2ca9794b593b33619ff63bc40da7641
Frederik Creemers <frederikcreemers@gmail.com>
2016-06-16 23:16:20 +0200

    better comments

--- src/com/project/base/Base.java
+++ src/com/project/base/Base.java
@@ -17,9 +17,12 @@ public class Base {
 			public void run() {
 				NagelMap m = new NagelMap();
 				//m.generateCircle(10, 10, 400, 400, 100);
+				
+				//If true, use the car-counting traffic lights.
+				//Otherwise, use the purely interval round-robin system.
 				m.setSmartIntersections(true);
 				
-				//Pas sin true here to add a CarSourceSink to the map
+				//Pass in true here to add a CarSourceSink to the map
 				m.generate(false);
 				//Pass in a different number to change the number of initial cars on the map.
 				m.addRandomCars(200);
